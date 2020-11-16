fb52f62abf79588ebb3b45f92e00f4b365195f29
fegegraphic <73474295+fegegraphic@users.noreply.github.com>
2020-11-16 12:56:09 +0100

    def

---
+++ js/java.js
@@ -0,0 +1,18 @@
+$ .scrollify ( {
+    section : ".box" ,
+    sectionName : "section-name" ,
+    interstitialSection : "" ,
+    easing : "easeOutExpo" ,
+    scrollSpeed : 1100 ,
+    offset : 0 ,
+    scrollbars : true ,
+    standardScrollElements : "" ,
+    setHeights : true ,
+    overflowScroll : true ,
+    updateHash :true ,
+    touchScroll : true ,
+    prima : function ( )  { } ,
+    dopo : function ( )  { } ,
+    afterResize : function ( )  { } ,
+    afterRender : function ( )  { }
+  } ) ;
